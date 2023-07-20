d69b735bf2ac15e97e312b3c157bb8e26cdfac12
Tony <tony@DESKTOP-IA5KR0G>
2023-07-20 14:20:36 -0400

    Complete exercise 1 and (potentially) 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, int) {
+    let arr = []
+    for (let i = 0; i < int; i++) {
+        arr.push(string);
+    }
+    return arr.join("");
 };
 
 // Do not edit below this line
